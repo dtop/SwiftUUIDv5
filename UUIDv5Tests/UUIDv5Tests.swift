726f355162332bc1404b241a827286abb31dadbf
Danilo Topalovic <danilo.topalovic@nerdsee.com>
2016-07-03 23:03:48 +0200

    more tools

--- UUIDv5Tests/UUIDv5Tests.swift
+++ UUIDv5Tests/UUIDv5Tests.swift
@@ -40,5 +40,32 @@ class UUIDv5Tests: XCTestCase {
             
             XCTAssertEqual(uuid.uuidString, testCase.expected)
         }
+        
+        let invalid = UUID(namespace: nsDns, name: "")
+        XCTAssertNil(invalid)
+    }
+    
+    func testUuidStringNamespace() {
+        
+        let ns = "6BA7B811-9DAD-11D1-80B4-00C04FD430C8"
+        
+        let tests: [(name: String, expected:String)] = [
+            ("foo", "7da78284-2f14-5e7f-95e1-baaa9027c26f".uppercased()),
+            ("bar", "a4fee5e3-abe2-5572-a781-ddc1564c067b".uppercased()),
+            ("baz", "3143abb9-bd5a-5a10-bbc6-2eca452be403".uppercased())
+        ]
+        
+        for testCase in tests {
+            
+            guard let uuid = UUID(namespaceString: ns, name: testCase.name) else {
+                XCTAssert(false)
+                return
+            }
+            
+            XCTAssertEqual(uuid.uuidString, testCase.expected)
+        }
+        
+        let invalid = UUID(namespaceString: "LoL", name: "foo")
+        XCTAssertNil(invalid)
     }
 }
